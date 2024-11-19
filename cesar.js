293920a0064c89147642cb9c72df2b70090e8500
Bruno Pereira <brunopereira@acad.ifma.edu.br>
2024-11-19 19:56:17 -0300

    Update cesar.js

--- cesar.js
+++ cesar.js
@@ -9,8 +9,7 @@ function criptografia(mensagem, chave, converter){
 function cifraLetra(letra, chave){
     let alfa = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     let letraConvertida
-    let tamVetor = alfa.length
-    let posLetra
+
     for(let i = 0; i<alfa.length;i++){
         if(letra == alfa[i]){
             letraConvertida = alfa[i+chave]
