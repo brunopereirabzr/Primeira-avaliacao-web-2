a4e6ebc603508009536a9d7607ca311455de0830
Bruno Pereira <brunopereira@acad.ifma.edu.br>
2024-11-19 19:51:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 import cifra from './cesar.js'
 
-let teste = cifra.cifraLetra('a',3)
-let teste2 = cifra.decifrarLetra('z',3)
+let cripto =  cifra.criptografia("bruno",2,cifra.cifraLetra)
 
-let teste3 =  cifra.criptografia("bruno",2,cifra.cifraLetra)
-
-console.log(teste)
-console.log(teste2)
-console.log(teste3)
+console.log(cripto)
